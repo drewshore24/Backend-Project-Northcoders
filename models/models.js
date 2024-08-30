b1863c466c4cc97231ce8e7b024d97eb792bd061
Drew <andrewshore123@hotmail.com>
2024-08-30 11:22:57 +0100

    Question 13 get/api/articles/:article_id (comment_count)

--- models/models.js
+++ models/models.js
@@ -19,7 +19,7 @@ const selectEndpoint = () => {
 
 const selectArticleID = (id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [id])
+    .query("SELECT articles.title, articles.author, articles.body, articles.created_at, articles.votes, articles.article_img_url, articles.topic, COUNT (comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id, comments.comment_id", [id])
     .then((data) => {
       if (data.rows.length === 0) {
         return Promise.reject({
@@ -157,3 +157,5 @@ module.exports = {
   selectCommentID,
   selectUsers,
 };
+
+
